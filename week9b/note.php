cd7d79f84b9ee1fe99c32f6accc75b51f704bff3
Leila Arcega <lbarcega@student.apc.edu.ph>
2023-02-16 15:27:53 +0800

    updated note.php

--- week9b/note.php
+++ week9b/note.php
@@ -10,28 +10,28 @@
   <title>Notes</title>
 </head>
   <body class="profile">
-  <a href="profile.php"><img class="bg-image" src="images/logo-bg.png" alt="the boy"></a>
+  <img class="bg-image" src="images/logo-bg.png" alt="the boy">
     <div class="container">
     <?php
       $note_id = $_GET['id'];
-
+      echo $note_id;
       //1. Setup Database connection
-      $servername = "localhost";
-      $db_username = "root";
-      $db_password = "";
-      $database = "ajah_wbdb";
+      $servername = "192.168.150.213";
+      $db_username = "webprogmi212";
+      $db_password = "b3ntRhino98";
+      $database = "webprogmi212";
 
       $conn = mysqli_connect($servername, $db_username, $db_password, $database);
-
+  
       //2. SELECT SQL
-      $sql = "SELECT * FROM `notes` WHERE note_id='" . $note_id . "'";
+      $sql = "SELECT * FROM `lbarcega_notes` WHERE note_id='" . $note_id . "'";
 
       //Either 1 or zero result
 
       //3. Execute SQL
       $result = mysqli_query($conn, $sql);
       $row = mysqli_fetch_array($result); // mysqli_fetch_array is good for result that is 1 or 0 record
-
+      mysqli_close($conn);
       echo '
 			<div class="small-container"">
 				<div class="gallery">
